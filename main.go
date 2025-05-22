b8838dea8593415d25fe73f5476b91f02ad2618e
LucaFe1337 <luca.ferlisi@web.de>
2025-05-22 20:29:27 +0200

    Simple routing

--- main.go
+++ main.go
@@ -3,10 +3,47 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	accessNum := cfg.fileserverHits.Load()
+	fmt.Fprintf(w, "Hits: %d", accessNum)
+}
+
+func (cfg *apiConfig) resetMetrics(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Count reset!")
+}
 
 func main() {
 	mux := http.NewServeMux()
+
+	fs := http.FileServer(http.Dir("."))
+
+	apiCfg := apiConfig{}
+
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", fs)))
+	mux.HandleFunc("GET /metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /reset", apiCfg.resetMetrics)
+
+	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
 
 	server := &http.Server{
 		Addr:    ":8080",
@@ -18,4 +55,5 @@ func main() {
 	if err != nil {
 		fmt.Println("Server error", err)
 	}
+
 }
