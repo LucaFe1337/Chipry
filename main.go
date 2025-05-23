9d673dcae66ca1b0418c30c7658932fda0f7de93
LucaFe1337 <luca.ferlisi@web.de>
2025-05-23 18:30:21 +0200

    added validation for chirps

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 	"sync/atomic"
 )
 
@@ -19,13 +21,102 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 
 func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
 	accessNum := cfg.fileserverHits.Load()
-	fmt.Fprintf(w, "Hits: %d", accessNum)
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	htmlResponse := fmt.Sprintf(`
+	<html>
+  		<body>
+    		<h1>Welcome, Chirpy Admin</h1>
+    		<p>Chirpy has been visited %d times!</p>
+  		</body>
+	</html>`, accessNum)
+	fmt.Fprint(w, htmlResponse)
 }
 
 func (cfg *apiConfig) resetMetrics(w http.ResponseWriter, r *http.Request) {
 	cfg.fileserverHits.Store(0)
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "Count reset!")
+}
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	type errorResponse struct {
+		Error string `json:"error"`
+	}
+	resp := errorResponse{Error: message}
+	dat, err := json.Marshal(resp)
+	if err != nil {
+		fmt.Printf("Error marshaling JSON error response: %s\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(dat)
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	dat, err := json.Marshal(payload)
+	if err != nil {
+		fmt.Printf("Error marshaling JSON response: %s\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(dat)
+}
+
+func validateChirps(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	param := parameters{}
+	err := decoder.Decode(&param)
+	if err != nil {
+		// Fehler beim Dekodieren des JSON-Bodies
+		// Der Client hat ungültiges JSON gesendet
+		respondWithError(w, http.StatusBadRequest, "Invalid JSON format")
+		return
+	}
+
+	chirpText := param.Body
+	maxLength := 140
+
+	if len(chirpText) > maxLength {
+		// Chirp ist zu lang
+		respondWithError(w, http.StatusBadRequest, fmt.Sprintf("Chirp is too long. Max length is %d characters.", maxLength))
+		return
+	}
+	cleanedChirp := cleanChirp(chirpText)
+
+	type successResponse struct {
+		Cleaned_Body string `json:"cleaned_body"`
+	}
+
+	resp := successResponse{
+		Cleaned_Body: cleanedChirp,
+	}
+
+	respondWithJSON(w, http.StatusOK, resp)
+}
+
+func cleanChirp(text string) string {
+	badWords := map[string]string{
+		"kerfuffle": "****",
+		"sharbert":  "****",
+		"fornax":    "****",
+		"Kerfuffle": "****",
+		"Sharbert":  "****",
+		"Fornax":    "****",
+	}
+
+	cleanedText := text
+	for bad, replacement := range badWords {
+		cleanedText = strings.ReplaceAll(cleanedText, bad, replacement)
+	}
+	return cleanedText
 }
 
 func main() {
@@ -36,10 +127,11 @@ func main() {
 	apiCfg := apiConfig{}
 
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", fs)))
-	mux.HandleFunc("GET /metrics", apiCfg.handlerMetrics)
-	mux.HandleFunc("POST /reset", apiCfg.resetMetrics)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /admin/reset", apiCfg.resetMetrics)
+	mux.HandleFunc("POST /api/validate_chirp", validateChirps)
 
-	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
