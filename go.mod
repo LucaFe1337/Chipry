8cbea60e5c0fc71c989dbb035da578e957f13e05
LucaFe1337 <luca.ferlisi@web.de>
2025-05-26 14:49:58 +0200

    added extra functionalities, adding users, deleting all

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/LucaFe1337/Chipry
 
 go 1.24.1
+
+require (
+	github.com/google/uuid v1.6.0
+	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.9
+)
--- main.go
+++ main.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
 	"strings"
 	"sync/atomic"
+	"time"
+
+	"github.com/LucaFe1337/Chipry/internal/database"
+	"github.com/google/uuid"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	DB             *database.Queries
+	PLATFORM       string
+}
+
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Email     string    `json:"email"`
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
@@ -33,9 +50,19 @@ func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
 }
 
 func (cfg *apiConfig) resetMetrics(w http.ResponseWriter, r *http.Request) {
-	cfg.fileserverHits.Store(0)
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Count reset!")
+	if cfg.PLATFORM == "dev" {
+		cfg.fileserverHits.Store(0)
+		err := cfg.DB.DeleteAllUsers(r.Context())
+		if err != nil {
+			fmt.Fprint(w, "Error deleting users!")
+		}
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "Count reset!")
+	} else {
+		w.WriteHeader(http.StatusForbidden)
+		return
+	}
+
 }
 
 func respondWithError(w http.ResponseWriter, code int, message string) {
@@ -119,17 +146,58 @@ func cleanChirp(text string) string {
 	return cleanedText
 }
 
+func (cfg *apiConfig) createNewUser(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Email string `json:"email"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	param := parameters{}
+	err := decoder.Decode(&param)
+	if err != nil {
+		// Fehler beim Dekodieren des JSON-Bodies
+		// Der Client hat ungültiges JSON gesendet
+		respondWithError(w, http.StatusBadRequest, "Invalid JSON format")
+		return
+	}
+
+	user, err := cfg.DB.CreateUser(r.Context(), param.Email)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "smth went wrong Creating the user!")
+		return
+	}
+	resp := User{
+		ID:        user.ID,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+		Email:     user.Email,
+	}
+	respondWithJSON(w, http.StatusCreated, resp)
+}
+
 func main() {
+	godotenv.Load()
 	mux := http.NewServeMux()
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		fmt.Println("Error in DB!", err)
+	}
+	dbQueries := database.New(db)
+	platform := os.Getenv("PLATFORM")
 
 	fs := http.FileServer(http.Dir("."))
 
-	apiCfg := apiConfig{}
+	apiCfg := apiConfig{
+		DB:       dbQueries,
+		PLATFORM: platform,
+	}
 
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", fs)))
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.resetMetrics)
 	mux.HandleFunc("POST /api/validate_chirp", validateChirps)
+	mux.HandleFunc("POST /api/users", apiCfg.createNewUser)
 
 	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
@@ -143,7 +211,7 @@ func main() {
 	}
 
 	fmt.Println("Server is listening on localhost:8080")
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	if err != nil {
 		fmt.Println("Server error", err)
 	}
